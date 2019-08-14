ec90ab3c3b10aa21b26226701953a29cfc8dcaa1
Charles DeLeo <charles.deleo979@gmail.com>
2019-08-14 21:01:43 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,6 +3,6 @@
 def #meal_choice(veg1, veg2, protein = "meat")
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end 
-def #meal_choice(vegan)
+def #meal_choice(veg1, veg2, protein = "vegan")
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
