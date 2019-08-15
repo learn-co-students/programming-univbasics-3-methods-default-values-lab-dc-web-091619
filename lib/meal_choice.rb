61843e40ead93ce854762475072143173ce896c0
Graham Flaspoehler <grahamflas@gmail.com>
2019-08-15 00:10:08 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,8 @@
 def meal_choice(veg1, veg2, protein = "meat")
-    puts "What a nutritious meal!"
     meal = "A plate of #{protein} with #{veg1} and #{veg2}."
+
+    puts "What a nutritious meal!"
     puts meal
+    
+    return meal
 end
