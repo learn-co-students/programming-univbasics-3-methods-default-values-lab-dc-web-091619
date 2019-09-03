e168e1ec37307cdade5003ebc6c36e62da2c3137
Marcos Velasco <devslife7@gmail.com>
2019-09-03 17:53:15 -0400

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,9 @@
 # Your code here
+
+
+def meal_choice(veggie, calories, protein = "meat")
+    output = "A plate of #{protein} with #{veggie} and #{calories}."
+    puts "What a nutritious meal!"
+    puts output
+    return output
+end
